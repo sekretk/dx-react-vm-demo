12eb1391b307b74fd0f7311481105837b5a07138
ksekret <ksekret@sekret-mac.office.devexperts.com>
2021-10-24 18:06:11 +0300

    init

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -6,7 +6,7 @@ const commonConfig = require("./common");
 
 module.exports = merge(commonConfig, {
   mode: "production",
-  entry: "./index.tsx",
+  entry: "./index.ts",
   output: {
     filename: "js/bundle.[contenthash].min.js",
     path: resolve(__dirname, "../../dist"),
