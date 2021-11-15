7b405f242e69127f0d935eb8306489b94c0a1cc0
ksekret <sekret.constantine@gmail.com>
2021-11-15 09:58:05 +0300

    is string literal

--- src/types/generic.ts
+++ src/types/generic.ts
@@ -1,5 +1,15 @@
+import { IfStrictEquals, IsFalse, IsTrue } from "./test-utils";
+
 export type TypeCheckError<T extends string> = {
     Error: T
 }
 
 export type KeyValueTuple<T, Y extends string> = [Y, T];
+
+export type IsStringLiteral<T> = IfStrictEquals<T, string, false, true>;
+
+type TEST_CASE_IS_STRING_LITERAL = [
+    IsTrue<IsStringLiteral<'sss'>>,
+    IsFalse<IsStringLiteral<string>>,
+    IsTrue<IsStringLiteral<''>>,
+]
--- src/types/test-utils.ts
+++ src/types/test-utils.ts
@@ -1,6 +1,6 @@
 export type IsTrue<T extends true> = T;
 export type IsFalse<T extends false> = T;
 
-export type IfStrictEquals<T, U, TOnTrue = true, TonFalse = false> =
+export type IfStrictEquals<T, U, TOnTrue = true, TOnFalse = false> =
   (<G>() => G extends T ? 1 : 2) extends
-  (<G>() => G extends U ? 1 : 2) ? TOnTrue : TonFalse;
+  (<G>() => G extends U ? 1 : 2) ? TOnTrue : TOnFalse;
